676b7df64cfbec3971a88f1831afdc6d754e275a
Bibianx <ladytorres@ierv.edu.co>
2025-12-12 11:02:12 -0500

    feat(Validators): Se crea validador para el dto de categorias

---
+++ Validators/CategoriesValidator.cs
@@ -0,0 +1,16 @@
+using FluentValidation;
+using Models.Dtos;
+
+public class CreateProductDtoValidator : AbstractValidator<CategoriesRequest>
+{
+    public CreateProductDtoValidator()
+    {
+        RuleFor(x => x.CategoryName)
+            .NotEmpty().WithMessage("El nombre es obligatorio")
+            .Length(3, 100).WithMessage("Debe tener entre 3 y 100 caracteres");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("La descripción es obligatoria")
+            .Length(5, 100).WithMessage("Debe tener entre 5 y 100 caracteres");
+    }
+}
